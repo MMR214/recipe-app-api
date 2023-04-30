15dd61b9b54f7c46af9442002af401d72b119f7f
Mick <mickriego214@gmail.com>
2023-04-30 21:22:49 +0400

    admin test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 class ModelTest(TestCase):
     """Test Models."""
 
-    def test_create_user_email_successful(self):
+    def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email ='test@example.com'
         password ='testpass123'
@@ -25,7 +25,7 @@ class ModelTest(TestCase):
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
